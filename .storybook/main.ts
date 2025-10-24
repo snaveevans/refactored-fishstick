4b8622c2088599ff4d700bc2efa3c322b042db46
Tyler Evans <snaveevans@users.noreply.github.com>
2025-10-24 09:41:46 -0600

    fix linting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ import type { StorybookConfig } from 'storybook-react-rsbuild';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 
@@ -22,7 +22,9 @@ const config: StorybookConfig = {
     },
   ],
   framework: {
-    name: getAbsolutePath('storybook-react-rsbuild'),
+    name: getAbsolutePath(
+      'storybook-react-rsbuild',
+    ) as 'storybook-react-rsbuild',
     options: {},
   },
   typescript: {
